7e77552537195db7a4b8d6a337789a70d002e51b
paramita.a.pradhan@outlook.com <paramita.a.pradhan@outlook.com>
2019-10-08 11:44:10 -0400

    Add project files.

---
+++ HelloXamarin/HelloXamarin/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using HelloXamarin.Services;
+using HelloXamarin.Views;
+
+namespace HelloXamarin
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
